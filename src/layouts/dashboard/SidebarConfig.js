b3fca23558d58cf0db5e5fb64770955f953069fe
Hoai Thanh <hoaithanh@HeavenlySwords-Mac.local>
2024-05-24 17:59:10 +0700

    init

---
+++ src/layouts/dashboard/SidebarConfig.js
@@ -0,0 +1,59 @@
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+// components
+import SvgIconStyle from '../../components/SvgIconStyle';
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name) => (
+  <SvgIconStyle src={`/static/icons/navbar/${name}.svg`} sx={{ width: '100%', height: '100%' }} />
+);
+
+const ICONS = {
+  blog: getIcon('ic_blog'),
+  cart: getIcon('ic_cart'),
+  chat: getIcon('ic_chat'),
+  mail: getIcon('ic_mail'),
+  user: getIcon('ic_user'),
+  kanban: getIcon('ic_kanban'),
+  banking: getIcon('ic_banking'),
+  calendar: getIcon('ic_calendar'),
+  ecommerce: getIcon('ic_ecommerce'),
+  analytics: getIcon('ic_analytics'),
+  dashboard: getIcon('ic_dashboard'),
+  booking: getIcon('ic_booking')
+};
+
+const sidebarConfig = [
+  {
+    subheader: 'management',
+    items: [
+      {
+        title: 'user',
+        path: PATH_DASHBOARD.user.root,
+        icon: ICONS.user,
+        children: [
+          { title: 'list', path: PATH_DASHBOARD.user.list },
+          { title: 'create', path: PATH_DASHBOARD.user.newUser },
+          { title: 'edit', path: PATH_DASHBOARD.user.editById },
+          { title: 'account', path: PATH_DASHBOARD.user.account }
+        ]
+      },
+
+      // MANAGEMENT : E-COMMERCE
+      {
+        title: 'e-commerce',
+        path: PATH_DASHBOARD.eCommerce.root,
+        icon: ICONS.cart,
+        children: [
+          { title: 'product', path: PATH_DASHBOARD.eCommerce.productById },
+          { title: 'list', path: PATH_DASHBOARD.eCommerce.list },
+          { title: 'create', path: PATH_DASHBOARD.eCommerce.newProduct },
+          { title: 'edit', path: PATH_DASHBOARD.eCommerce.editById }
+        ]
+      }
+    ]
+  }
+];
+
+export default sidebarConfig;
